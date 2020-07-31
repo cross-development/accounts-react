ca54c4b1953ab5674b08b5a9542698c102b45014
Vitaliy Derda <derdavitaliy@gmail.com>
2020-08-01 00:21:14 +0300

    added app and search form

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,15 @@
+//Core
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+//Styles
+import styles from './SearchForm.module.css';
+
+export default class SearchForm extends Component {
+	static propTypes = {};
+
+	state = {};
+
+	render() {
+		return <div></div>;
+	}
+}
